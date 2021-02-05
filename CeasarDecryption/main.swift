5c9e7bc574611d8490906546bae808ffa500c3fe
Frank <frank@steamedveggie.com>
2021-02-05 11:22:53 -0600

    Write Script

--- CeasarDecryption/main.swift
+++ CeasarDecryption/main.swift
@@ -7,5 +7,27 @@
 
 import Foundation
 
-print("Hello, World!")
+print("This program will decrypt the ceasar encryption in Challenge #1.");
+
+let codes:[String] = ["UCFFNG", "TFBUQPTU", "GVERO", "CNKKR", "GFWXYJR", "FKDLQ"];
+
+for c in codes {
+    var curr: String = c;
+    print(" -> \(c)");
+    for _ in 0...26 {
+        var temp: String = "";
+        for ch in curr {
+            var v: Int = Int(ch.unicodeScalars.map{$0.value}.reduce(0, +)) + 1;
+            if v == 91 {
+                v = 65;
+            }
+            temp.append(Character(UnicodeScalar(v)!));
+        }
+        curr = temp;
+        print(" ---> \(curr)");
+        if curr.starts(with:"BAR") {
+            print("Posible answer: \(curr)");
+        }
+    }
+}
 
